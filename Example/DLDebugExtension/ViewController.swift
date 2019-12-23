a9f7e902d131b1a9fb7dabe2db41ed48682999d7
Daniel Lin <linzhdk@gmail.com>
2019-12-23 09:46:34 +0800

    build: update the example project to support for Xcode 11.3 and Swift 5.0, and pod update

--- Example/DLDebugExtension/ViewController.swift
+++ Example/DLDebugExtension/ViewController.swift
@@ -6,10 +6,10 @@
 //  Copyright (c) 2018 dklinzh. All rights reserved.
 //
 
+import DLDebugExtension
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func debugAction(_ sender: UIBarButtonItem) {
+        self.debug.showModeOptions()
+    }
 }
-
